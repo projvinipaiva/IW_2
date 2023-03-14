aadbfb2df4dc396fdac30ca40a83e0715968000c
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2023-03-14 11:08:36 -0300

    Add files via upload

---
+++ modulo/calc.js
@@ -0,0 +1,9 @@
+const somaF = require('./soma')
+const subF = require('./sub')
+const divF = require('./div')
+const multF = require('./mult')
+
+console.log("A soma é: " + somaF(3,3))
+console.log("A divisão é: " + divF(3,3))
+console.log("A multiplicação é: " + multF(3,3))
+console.log("A subtração é: " + subF(3,3))
